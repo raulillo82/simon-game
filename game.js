5adf83e1bcf8d9f794bad3bf8107c51ed14c9496
Raúl Osuna <raulosuna@gmail.com>
2022-03-17 01:41:24 +0100

    First step of the game added, changing header

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
 
+//Detect cliks on any of the buttons
 $(".btn").on("click", function() {
   //Get the clicked button
   var userChosenColour = $(this).attr("id");
@@ -13,7 +16,25 @@ $(".btn").on("click", function() {
   animatePress(userChosenColour);
 });
 
+//Detect key press
+$(document).on("keypress", function() {
+  //Only start the game in the first key press.
+  if (!started) {
+    //Change title.
+    $("#level-title").text("Level " + level);
+    //Generate the next sequence in the pattern.
+    nextSequence();
+    //Make sure the game is not started again
+    started = true;
+  }
+});
+
 function nextSequence() {
+  //Increase the Level
+  level ++;
+  //Change title.
+  $("#level-title").text("Level " + level);
+
   //4 possible colours, generate a random number for the index
   var randomNumber = Math.floor(Math.random() * 4);
   //Use the previous random index for selecting a real colour.
@@ -25,16 +46,18 @@ function nextSequence() {
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
   //Play the corresponding sound
-  audio_name = "sounds/" + randomChosenColour + ".mp3";
-  audio.play();
+  audioName = "sounds/" + randomChosenColour + ".mp3";
+  playSound(audioName);
 }
 
 function playSound(name) {
-  var audio = new Audio(name);
-  audio.play();
+  var audioName = new Audio(name);
+  audioName.play();
 }
 
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
-  setTimeout(function () {$("#" + currentColour).removeClass("pressed");}, 100);
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
